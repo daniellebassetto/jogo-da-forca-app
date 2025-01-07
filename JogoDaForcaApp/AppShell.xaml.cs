e7e2b59e7c0f543a978443ad0edda28a59734239
Danielle Bassetto <danibbassetto@hotmail.com>
2025-01-06 22:27:45 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDaForcaApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JogoDaForcaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ JogoDaForcaApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JogoDaForcaApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
